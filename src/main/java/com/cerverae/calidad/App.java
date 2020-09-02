884b247f761082ccf3b879bf2b5ad3647595caa3
Esteban Cervera <cerverae18@hotmail.com>
2020-09-01 19:33:50 -0500

    first commit

---
+++ src/main/java/com/cerverae/calidad/App.java
@@ -0,0 +1,13 @@
+package com.cerverae.calidad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
